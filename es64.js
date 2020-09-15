9acbf7276075d4202a11fb20db091b842d420383
Dzulfanni <belajarayok866@gmail.com>
2020-09-15 22:14:13 +0700

    first commit

---
+++ es64.js
@@ -0,0 +1,10 @@
+//no4
+
+const prabu = ["Rizal", "Prabumulih", "SMKprabumulih","prabujaya","sukajadi"];
+const [nama, asal, ...rest]=prabu
+
+
+console.log(nama);
+console.log(asal);
+console.log(...rest);
+
